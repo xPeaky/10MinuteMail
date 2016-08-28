bfb87b4ca7001807b7ccda49b18f4021d442a70b
Baiku <baiku1@hotmail.fr>
2016-08-28 10:58:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10MinuteMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mail = new TenMinuteMail();
+            mail.Initialize();
+            mail.Interval = 2000;
+            mail.OnReceiveMail += Mail_OnReceiveMail;
+            Console.WriteLine(mail.GetEmailAddress());
+            mail.Start();
+
+            Console.Read();
+        }
+
+        private static void Mail_OnReceiveMail(object sender, MailEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\New message !");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Attachment Count: {e.Mail.AttachmentCount}");
+            Console.WriteLine("Attachments: ");
+
+            foreach(var item in e.Mail.Attachments)
+            {
+                Console.WriteLine($"-{item}");
+            }
+
+            Console.WriteLine($"BodyPlainText: {e.Mail.BodyPlainText}");
+            Console.WriteLine($"BodyPreview: {e.Mail.BodyPreview}");
+            Console.WriteLine($"BodyText: {e.Mail.BodyText}");
+            Console.WriteLine($"FormattedDate: {e.Mail.FormattedDate}");
+            Console.WriteLine($"Forwarded: {e.Mail.Forwarded}");
+            Console.WriteLine("FromList: ");
+
+            foreach(var item in e.Mail.FromList)
+            {
+                Console.WriteLine($"-{item}");
+            }
+
+            Console.WriteLine($"ID: {e.Mail.ID}");
+            Console.WriteLine($"PrimaryFromAddress: {e.Mail.PrimaryFromAddress}");
+            Console.WriteLine($"Read: {e.Mail.Read}");
+            Console.WriteLine("RecipientList: ");
+
+            foreach(var item in e.Mail.RecipientList)
+            {
+                Console.WriteLine($"-{item}");
+            }
+
+            Console.WriteLine($"RepliedTo: {e.Mail.RepliedTo}");
+            Console.WriteLine($"SentDate: {e.Mail.SentDate}");
+            Console.WriteLine($"Subject: {e.Mail.Subject}");
+        }
+    }
+}
---
+++ TenMinuteMail.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace _10MinuteMail
+{
+    public class TenMinuteMail
+    {
+
+        private string Email { get; set; }
+        private CookieContainer cookies;
+        private int messageCount;
+        private List<Mail> mails;
+        public int Interval { get; set; }
+
+        public delegate void ReceiveMailHandler(object sender, MailEventArgs e);
+        public event ReceiveMailHandler OnReceiveMail;
+
+        private bool started = false;
+
+        public string GetEmailAddress()
+        {
+            return Email;
+        }
+
+        public void Initialize()
+        {
+            var req = FastHttpRequest.Create("https://10minutemail.com/10MinuteMail/index.html");
+            cookies = new CookieContainer();
+            messageCount = 0;
+
+            if (Interval == 0)
+                Interval = 1000;
+
+            mails = new List<Mail>();
+
+            req.CookieContainer = cookies;
+            using (var response = (HttpWebResponse)req.GetResponse())
+            {
+                cookies.Add(response.Cookies);
+            }
+
+            req = FastHttpRequest.Create("https://10minutemail.com/10MinuteMail/resources/session/address");
+            req.CookieContainer = cookies;
+
+            using (var response = (HttpWebResponse)req.GetResponse())
+            {
+                var reader = new StreamReader(response.GetResponseStream());
+                Email = reader.ReadToEnd();
+            }
+        }
+
+        public int GetMessageCount()
+        {
+            var req = FastHttpRequest.Create("https://10minutemail.com/10MinuteMail/resources/messages/messageCount");
+            req.CookieContainer = cookies;
+
+            int i;
+
+            using (var response = (HttpWebResponse)req.GetResponse())
+            {
+                var reader = new StreamReader(response.GetResponseStream());
+                i = Convert.ToInt32(reader.ReadToEnd());
+            }
+
+            return i;
+        }
+
+        public bool CheckMail()
+        {
+            if (GetMessageCount() != messageCount)
+                return true;
+
+            return false;
+        }
+
+        public List<Mail> GetMails()
+        {
+            var req = FastHttpRequest.Create("https://10minutemail.com/10MinuteMail/resources/messages/messagesAfter/0");
+            req.CookieContainer = cookies;
+
+            string json;
+            using (var response = (HttpWebResponse)req.GetResponse())
+            {
+                var reader = new StreamReader(response.GetResponseStream());
+                json = reader.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<List<Mail>>(json);
+        }
+
+        public Mail GetFirstMail()
+        {
+            return mails[0];
+        }
+
+        public Mail GetLastMail()
+        {
+            return mails[mails.Count - 1];
+        }
+
+        public Mail GetMail()
+        {
+            int i = messageCount;
+            if (i != 0)
+                i--;
+
+            var req = FastHttpRequest.Create($"https://10minutemail.com/10MinuteMail/resources/messages/messagesAfter/{i}");
+            req.CookieContainer = cookies;
+
+            string json;
+            using (var response = (HttpWebResponse)req.GetResponse())
+            {
+                var reader = new StreamReader(response.GetResponseStream());
+                json = reader.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<List<Mail>>(json)[0];
+        }
+
+        public void Start()
+        {
+            started = true;
+            Task.Run(() =>
+            {
+                while (started)
+                {
+                    if (CheckMail())
+                    {
+                        messageCount = GetMessageCount();
+                        var mail = GetMail();
+                        OnReceiveMail(this, new MailEventArgs(mail));
+                    }
+
+                    System.Threading.Thread.Sleep(Interval);
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            started = false;
+        }
+
+    }
+
+    public class Mail
+    {
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+        [JsonProperty("repliedTo")]
+        public bool RepliedTo { get; set; }
+        [JsonProperty("forwarded")]
+        public bool Forwarded { get; set; }
+        [JsonProperty("sentDate")]
+        public long SentDate { get; set; }
+        [JsonProperty("recipientList")]
+        public string[] RecipientList { get; set; }
+        [JsonProperty("fromList")]
+        public string[] FromList { get; set; }
+        [JsonProperty("bodyText")]
+        public string BodyText { get; set; }
+        [JsonProperty("bodyPlainText")]
+        public string BodyPlainText { get; set; }
+        [JsonProperty("formattedDate")]
+        public string FormattedDate { get; set; }
+        [JsonProperty("bodyPreview")]
+        public string BodyPreview { get; set; }
+        [JsonProperty("attachmentCount")]
+        public int AttachmentCount { get; set; }
+        [JsonProperty("attachments")]
+        public string[] Attachments { get; set; }
+        [JsonProperty("read")]
+        public bool Read { get; set; }
+        [JsonProperty("id")]
+        public string ID { get; set; }
+        [JsonProperty("primaryFromAddress")]
+        public string PrimaryFromAddress { get; set; }
+    }
+
+    public class MailEventArgs : EventArgs
+    {
+        public Mail Mail { get; set; }
+
+        public MailEventArgs(Mail mail)
+        {
+            this.Mail = mail;
+        }
+    }
+}
